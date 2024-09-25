70b4d7144c1122144ad98fd0a25c8c1b2d51a9eb
ngthu1995 <ngthu1995@gmail.com>
2024-09-25 14:41:31 -0400

    complete update user profile image with supabase

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,11 +1,12 @@
 "use server";
 
-import { profileSchema, validateWithZodSchema } from "./schemas";
+import { profileSchema, imageSchema, validateWithZodSchema } from "./schemas";
 
 import db from "./db";
 import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { uploadImage } from "@/utils/supabase";
 
 const getAuthUser = async () => {
   const user = await currentUser();
@@ -103,6 +104,24 @@ export const updateProfileAction = async (
 export const updateProfileImageAction = async (
   prevState: any,
   formData: FormData
-): Promise<{ message: string }> => {
-  return { message: "Profile image updated successfully" };
+) => {
+  const user = await getAuthUser();
+  try {
+    const image = formData.get("image") as File;
+    const validatedFields = validateWithZodSchema(imageSchema, { image });
+    const fullPath = await uploadImage(validatedFields.image);
+
+    await db.profile.update({
+      where: {
+        clerkId: user.id,
+      },
+      data: {
+        profileImage: fullPath,
+      },
+    });
+    revalidatePath("/profile");
+    return { message: "Profile image updated successfully" };
+  } catch (error) {
+    return renderError(error);
+  }
 };
