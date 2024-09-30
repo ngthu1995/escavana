25eb206f5db43ec93d047afee517313e9004a382
ngthu1995 <ngthu1995@gmail.com>
2024-09-30 07:53:26 -0400

    add loading cards for better UI experience, add nav search functionality in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import CategoriesList from "@/components/home/CategoriesList";
 import PropertiesContainer from "@/components/home/PropertiesContainer";
+import { Suspense } from "react";
+import LoadingCards from "@/components/card/LoadingCards";
 
 export default function HomePage({
   searchParams,
@@ -13,10 +15,12 @@ export default function HomePage({
         category={searchParams?.category}
         search={searchParams?.search}
       />
-      <PropertiesContainer
-        category={searchParams?.category}
-        search={searchParams?.search}
-      />
+      <Suspense fallback={<LoadingCards />}>
+        <PropertiesContainer
+          category={searchParams?.category}
+          search={searchParams?.search}
+        />
+      </Suspense>
     </section>
   );
 }
