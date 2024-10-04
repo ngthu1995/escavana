576fd913cb40990d3f6239a82a6af16c6480577b
ngthu1995 <ngthu1995@gmail.com>
2024-10-04 13:36:34 -0400

    add booking calendar

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -3,6 +3,9 @@ import { redirect } from "next/navigation";
 import BreadCrumbs from "@/components/properties/BreadCrumbs";
 import FavoriteToggleButton from "@/components/card/FavoriteToggleButton";
 import ShareButton from "@/components/properties/ShareButton";
+import ImageContainer from "@/components/properties/ImageContainer";
+import BookingCalendar from "@/components/properties/BookingCalendar";
+import PropertyRating from "@/components/card/PropertyRating";
 
 async function PropertyDetailsPage({ params }: { params: { id: string } }) {
   const property = await fetchPropertyDetails(params.id);
@@ -19,6 +22,18 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
           <FavoriteToggleButton propertyId={property.id} />
         </div>
       </header>
+      <ImageContainer mainImage={property.image} name={property.name} />
+      <section className="lg:grid lg:grid-cols-12 gap-x-12 mt-12">
+        <div className="lg:col-span-8">
+          <div className="flex gap-x-4 items-center">
+            <h1 className="text-xl font-bold">{property.name}</h1>
+            <PropertyRating inPage propertyId={property.id} />
+          </div>
+        </div>
+        <div className="lg:col-span-4 flex flex-col items-center">
+          <BookingCalendar />
+        </div>
+      </section>
     </section>
   );
 }
