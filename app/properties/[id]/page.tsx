1d6f59317ab4a6595c3b3f63e7609ddc1096fdce
ngthu1995 <ngthu1995@gmail.com>
2024-10-05 23:52:00 -0400

    use dynamic to render property map on client side

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -11,6 +11,16 @@ import Description from "@/components/properties/Description";
 import Amenities from "@/components/properties/Amenities";
 import PropertyRating from "@/components/card/PropertyRating";
 import { Separator } from "@/components/ui/separator";
+import dynamic from "next/dynamic";
+import { Skeleton } from "@/components/ui/skeleton";
+
+const DynamicMap = dynamic(
+  () => import("@/components/properties/PropertyMap"),
+  {
+    ssr: false,
+    loading: () => <Skeleton className="h-[400px] w-full" />,
+  }
+);
 
 async function PropertyDetailsPage({ params }: { params: { id: string } }) {
   const property = await fetchPropertyDetails(params.id);
@@ -42,6 +52,7 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
           <Separator className="mt-4" />
           <Description description={property.description} />
           <Amenities amenities={property.amenities} />
+          <DynamicMap countryCode={property.country} />;
         </div>
         <div className="lg:col-span-4 flex flex-col items-center">
           <BookingCalendar />
