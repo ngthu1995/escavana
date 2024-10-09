1b3f6433a63680f8a39a4b3432a441d1f91772c7
ngthu1995 <ngthu1995@gmail.com>
2024-10-09 14:52:24 -0400

    remove unused dependency

--- app/reviews/loading.tsx
+++ app/reviews/loading.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Card, CardHeader } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
 function loading() {
   return (
