f5c37e0975110c75f9bc85172a8f339b6cca6032
ngthu1995 <ngthu1995@gmail.com>
2024-10-14 14:00:33 -0400

    fix pipeline

--- components/booking/BookingCalendar.tsx
+++ components/booking/BookingCalendar.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { Calendar } from "@/components/ui/calendar";
 import { useEffect, useState } from "react";
-import { useToast } from "@/components/ui/use-toast";
 import { DateRange } from "react-day-picker";
 import { useProperty } from "@/utils/store";
 
